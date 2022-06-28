6a9413a15855a823707bde88bbc20483e3025a82
Rahil Mehta <rahilm97@hotmail.com>
2022-06-27 17:11:12 -0700

    Let player choose range of random number

--- Random_Guesser.py
+++ Random_Guesser.py
@@ -2,9 +2,11 @@ import random
 from termcolor import colored
 
 print("\033[1m" + "Welcome to Guess the Number!" + "\033[0m")
-name = input("What's your name?")
+name = input("What's your name? ")
 print("Hello " + name + ". How are you?")
-num = random.randint(0, 10)
+maxVal = input("What should the maximum number in the range be (>= 1)? " )
+
+num = random.randint(0, int(maxVal))
 guess = -1
 while guess != num:
     guess = input("Guess the number (press q to quit): ")
