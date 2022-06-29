bed9d08cedd9040792444016e08719bd4d629b6b
Rahil Mehta <rahilm97@hotmail.com>
2022-06-29 09:35:56 -0700

    Count number of guesses required

--- Random_Guesser.py
+++ Random_Guesser.py
@@ -8,12 +8,19 @@ maxVal = input("What should the maximum number in the range be (>= 1)? " )
 
 num = random.randint(0, int(maxVal))
 guess = -1
+numGuesses = 0
 while guess != num:
     guess = input("Guess the number (press q to quit): ")
+    numGuesses += 1
     if guess == 'q': break
 
     if int(guess) != num:
         print(colored("Sorry, your guess is wrong", "red"))
     else:
         print(colored("You guessed correctly! Congratulations!", "green"))
+        if numGuesses > 1:
+            print("You took " + str(numGuesses) + " tries")
+        else:
+            print("You took " + str(numGuesses) + " try")
+
         break
